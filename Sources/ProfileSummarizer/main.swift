8a5380f534afbdf8fde2475d00c0650db96a4f04
Bartosz Polaczyk <polac24@gmail.com>
2024-05-18 05:55:17 -0400

    Make POC of printing actions

--- Sources/ProfileSummarizer/main.swift
+++ Sources/ProfileSummarizer/main.swift
@@ -14,13 +14,30 @@ let parser = ProfileLineParser()
 let exporter = ProfileExplorer(path: inputFile, mode: .fullFile(path: inputFile), parser: parser)
 let semphore = DispatchSemaphore(value: 0)
 Task {
-    let stream = exporter.start()
-    for await context in stream {
-        print(context)
+    let stream = exporter.startOnBazelAction()
+    for try await contextErased in stream {
+        // TODO: make type-safe for performance
+        guard let context = contextErased as? ProfileContext else {
+            continue
+        }
+        print(context.actions.last!)
     }
     semphore.signal()
 }
 
 semphore.wait()
 
+
+
+extension ProfileExplorer {
+    func startOnBazelAction() -> any AsyncSequence {
+        var observedActions: [BazelAction] = []
+        return start().filter { context in
+            defer {
+                observedActions = context.actions
+            }
+            return observedActions != context.actions
+        }
+    }
+}
 
